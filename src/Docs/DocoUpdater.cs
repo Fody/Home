2a82d19ee84fc8ff22016efc3e429c12e523991a
Simon Cropp <simon.cropp@gmail.com>
2019-07-23 22:08:04 +1000

    move to MarkdownSnippets.MsBuild

--- src/Docs/DocoUpdater.cs
+++ src/Docs/DocoUpdater.cs
@@ -1,41 +1,8 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
-using MarkdownSnippets;
-using Xunit;
-using Xunit.Abstractions;
+﻿using Xunit.Abstractions;
 
 public class DocoUpdater :
     XunitLoggingBase
 {
-    [Fact]
-    public async Task Run()
-    {
-        var root = GitRepoDirectoryFinder.FindForFilePath();
-
-        List<Snippet> snippets = new List<Snippet>();
-
-        await snippets.AppendUrlsAsSnippets(
-            "https://raw.githubusercontent.com/Fody/Fody/master/FodyPackaging/Weaver.props",
-            "https://raw.githubusercontent.com/Fody/Fody/master/FodyPackaging/build/FodyPackaging.props",
-            "https://raw.githubusercontent.com/Fody/Fody/master/FodyPackaging/build/FodyPackaging.targets");
-
-        var pagesDir = Path.Combine(root, "pages");
-        PurgeDirectory(pagesDir);
-
-        var markdownProcessor = new DirectoryMarkdownProcessor(root);
-        markdownProcessor.IncludeSnippets(snippets);
-        markdownProcessor.Run();
-    }
-
-    static void PurgeDirectory(string pagesDir)
-    {
-        foreach (var page in Directory.EnumerateFiles(pagesDir, "*.md"))
-        {
-            File.Delete(page);
-        }
-    }
-
     public DocoUpdater(ITestOutputHelper output) :
         base(output)
     {
